790dcdaff604c30fa24c0cae1cb85664dd2f1ceb
Jeffrey Brown <brownjs03@gmail.com>
2019-08-29 06:40:41 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -1,3 +1,4 @@
+require 'pry'
 
 def hopper
 	programmer_hash = 
@@ -82,6 +83,7 @@ def adding_matz
         }
      }
 programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
+binding.pry
 programmer_hash
     
 end
